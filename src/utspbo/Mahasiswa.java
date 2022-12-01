90d68c6a6dd7e5253aed741ec54d9bfe2b31458c
Beni Permadani <94419695+benipermadani@users.noreply.github.com>
2022-12-01 12:59:00 +0800

    Membuat Atribut dan Behavisior

--- src/utspbo/Mahasiswa.java
+++ src/utspbo/Mahasiswa.java
@@ -10,5 +10,14 @@ package utspbo;
  * @author benipermadani
  */
 public class Mahasiswa {
+    String Nama;
+    String NPM;
+    double IPK;
     
+    void cetakInfo(){
+        
+        System.out.println("Nama    : "+Nama);
+        System.out.println("NPM     : "+NPM);
+        System.out.println("IPK     : "+IPK);
+    }
 }
